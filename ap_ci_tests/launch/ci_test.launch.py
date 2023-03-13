e95fb01fdc7123eb9605a1d6de09d3ca6aff28cb
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-03-13 01:10:29 +0000

    AP_DDS: add SITL param file for serial params.
    - Add dds.parm to config folder.
    - Load additional params when starting SITL.
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- ap_ci_tests/launch/ci_test.launch.py
+++ ap_ci_tests/launch/ci_test.launch.py
@@ -73,6 +73,9 @@ def generate_launch_description():
     pkg_ardupilot = f"{home}/Code/ros2/xrce-dds/ardupilot_ros2_ws/src/ardupilot"
     print(pkg_ardupilot)
   
+    pkg_ap_ci_tests = f"{home}/Code/ros2/xrce-dds/ardupilot_ros2_ws/src/ardupilot_ros2/ap_ci_tests"
+    print(pkg_ap_ci_tests)
+
     dds_profile = f"{pkg_ardupilot}/libraries/AP_DDS/dds_xrce_profile.xml"
     print(dds_profile)
 
@@ -109,6 +112,9 @@ def generate_launch_description():
     )
 
     # SITL and MAVProxy
+    dds_param = f"{pkg_ap_ci_tests}/config/dds.parm"
+    print(dds_param)
+
     sim_vehicle_cmd = f"{pkg_ardupilot}/Tools/autotest/sim_vehicle.py"
     sim_vehicle = ExecuteProcess(
         cmd=[
@@ -122,6 +128,7 @@ def generate_launch_description():
                 "--enable-xrce-dds ",
                 "-A ",
                 f'"--uartC=uart:{device}0" ',
+                f"--add-param-file={dds_param} "
                 "--console"
             ]
         ],
