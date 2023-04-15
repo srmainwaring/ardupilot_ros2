018887a769f4922cf88df8d9950bdbace9b6ef7f
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-04-15 17:16:36 +0100

    Gazebo: enable static code checks in ardupilot_gz_gazebo
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- ardupilot_gz/ardupilot_gz_gazebo/src/BasicSystem.cc
+++ ardupilot_gz/ardupilot_gz_gazebo/src/BasicSystem.cc
@@ -1,19 +1,16 @@
-/*
- * Copyright (C) 2022 Open Source Robotics Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
-*/
+// Copyright 2022 Open Source Robotics Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // We'll use a string and the gzmsg command below for a brief example.
 // Remove these includes if your plugin doesn't need them.
@@ -34,7 +31,7 @@ GZ_ADD_PLUGIN(
     gz::sim::System,
     ardupilot_gz_gazebo::BasicSystem::ISystemPostUpdate)
 
-namespace ardupilot_gz_gazebo 
+namespace ardupilot_gz_gazebo
 {
 
 void BasicSystem::PostUpdate(const gz::sim::UpdateInfo &_info,
@@ -46,4 +43,4 @@ void BasicSystem::PostUpdate(const gz::sim::UpdateInfo &_info,
   }
 }
 
-}  // namespace ardupilot_gz_gazeba
+}  // namespace ardupilot_gz_gazebo
--- ardupilot_gz/ardupilot_gz_gazebo/src/FullSystem.cc
+++ ardupilot_gz/ardupilot_gz_gazebo/src/FullSystem.cc
@@ -1,19 +1,16 @@
-/*
- * Copyright (C) 2022 Open Source Robotics Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
-*/
+// Copyright 2022 Open Source Robotics Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // We'll use a string and the gzmsg command below for a brief example.
 // Remove these includes if your plugin doesn't need them.
@@ -39,7 +36,7 @@ GZ_ADD_PLUGIN(
     ardupilot_gz_gazebo::FullSystem::ISystemReset
 )
 
-namespace ardupilot_gz_gazebo 
+namespace ardupilot_gz_gazebo
 {
 
 void FullSystem::Configure(const gz::sim::Entity &_entity,
@@ -69,7 +66,7 @@ void FullSystem::Update(const gz::sim::UpdateInfo &_info,
 }
 
 void FullSystem::PostUpdate(const gz::sim::UpdateInfo &_info,
-                            const gz::sim::EntityComponentManager &_ecm) 
+                            const gz::sim::EntityComponentManager &_ecm)
 {
   if (!_info.paused && _info.iterations % 1000 == 0)
   {
@@ -82,4 +79,4 @@ void FullSystem::Reset(const gz::sim::UpdateInfo &_info,
 {
   gzdbg << "ardupilot_gz_gazebo::FullSystem::Reset" << std::endl;
 }
-}  // namespace ardupilot_gz_gazeba
+}  // namespace ardupilot_gz_gazebo
