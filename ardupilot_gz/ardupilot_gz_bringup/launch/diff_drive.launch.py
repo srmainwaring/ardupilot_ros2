da0c586b49b8c80d2eead0cbe4148ea17b388e12
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-04-15 17:51:37 +0100

    Gazebo: enable static code checks in ardupilot_gz_bringup
    - Apply flake8 extend-ignore.
    - Format launch files with black.
    - Apply common linters.
    
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- ardupilot_gz/ardupilot_gz_bringup/launch/diff_drive.launch.py
+++ ardupilot_gz/ardupilot_gz_bringup/launch/diff_drive.launch.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Launch a diff-drive robot in Gazebo and Rviz."""
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -27,87 +28,82 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    pkg_project_bringup = get_package_share_directory(
-          'ardupilot_gz_bringup')
-    pkg_project_gazebo = get_package_share_directory(
-          'ardupilot_gz_gazebo')
-    pkg_project_description = get_package_share_directory(
-          'ardupilot_gz_description')
-    pkg_ros_gz_sim = get_package_share_directory(
-          'ros_gz_sim')
+    """Generate a launch description for a diff-drive robot."""
+    pkg_project_bringup = get_package_share_directory("ardupilot_gz_bringup")
+    pkg_project_gazebo = get_package_share_directory("ardupilot_gz_gazebo")
+    pkg_project_description = get_package_share_directory("ardupilot_gz_description")
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
 
     # Load the SDF file from "description" package
     sdf_file = os.path.join(
-          pkg_project_description,
-          'models',
-          'diff_drive',
-          'model.sdf')
-    with open(sdf_file, 'r') as infp:
+        pkg_project_description, "models", "diff_drive", "model.sdf"
+    )
+    with open(sdf_file, "r") as infp:
         robot_desc = infp.read()
 
     gz_sim_server = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
+        ),
         launch_arguments={
-            'gz_args':
-            "-v4 -s -r " + os.path.join(
-                pkg_project_gazebo,
-                'worlds',
-                'diff_drive.sdf'
-            )
+            "gz_args": "-v4 -s -r "
+            + os.path.join(pkg_project_gazebo, "worlds", "diff_drive.sdf")
         }.items(),
     )
 
     gz_sim_gui = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
-        launch_arguments={'gz_args': "-v4 -g"}.items(),
+            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
+        ),
+        launch_arguments={"gz_args": "-v4 -g"}.items(),
     )
 
     robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='both',
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
         parameters=[
-            {'use_sim_time': True},
-            {'robot_description': robot_desc},
-        ]
+            {"use_sim_time": True},
+            {"robot_description": robot_desc},
+        ],
     )
 
     # RViz
     rviz = Node(
-       package='rviz2',
-       executable='rviz2',
-       arguments=['-d', os.path.join(
-            pkg_project_bringup,
-            'config',
-            'diff_drive.rviz')],
-       condition=IfCondition(LaunchConfiguration('rviz'))
+        package="rviz2",
+        executable="rviz2",
+        arguments=[
+            "-d",
+            os.path.join(pkg_project_bringup, "config", "diff_drive.rviz"),
+        ],
+        condition=IfCondition(LaunchConfiguration("rviz")),
     )
 
     # Bridge
     bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        parameters=[{
-            'config_file': os.path.join(
-                  pkg_project_bringup,
-                  'config',
-                  'diff_drive_bridge.yaml'),
-            'qos_overrides./tf_static.publisher.durability': 'transient_local',
-        }],
-        output='screen'
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        parameters=[
+            {
+                "config_file": os.path.join(
+                    pkg_project_bringup, "config", "diff_drive_bridge.yaml"
+                ),
+                "qos_overrides./tf_static.publisher.durability": "transient_local",
+            }
+        ],
+        output="screen",
     )
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'rviz',
-            default_value='true',
-            description='Open RViz.'),
-        robot_state_publisher,
-        bridge,
-        gz_sim_server,
-        gz_sim_gui,
-        rviz
-    ])
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "rviz", default_value="true", description="Open RViz."
+            ),
+            robot_state_publisher,
+            bridge,
+            gz_sim_server,
+            gz_sim_gui,
+            rviz,
+        ]
+    )
--- ardupilot_gz/ardupilot_gz_bringup/launch/iris.launch.py
+++ ardupilot_gz/ardupilot_gz_bringup/launch/iris.launch.py
@@ -1,5 +1,20 @@
 # Copyright 2023 ArduPilot.org.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+# Adapted from https://github.com/gazebosim/ros_gz_project_template
+#
 # Copyright 2019 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +29,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Launch iris quadcopter in Gazebo and Rviz."""
+"""Launch an iris quadcopter in Gazebo and Rviz."""
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -30,91 +45,83 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    pkg_project_bringup = get_package_share_directory(
-          'ardupilot_gz_bringup')
-    pkg_project_gazebo = get_package_share_directory(
-          'ardupilot_gz_gazebo')
+    """Generate a launch description for a iris quadcopter."""
+    pkg_project_bringup = get_package_share_directory("ardupilot_gz_bringup")
+    pkg_project_gazebo = get_package_share_directory("ardupilot_gz_gazebo")
     # pkg_project_description = get_package_share_directory(
     #       'ardupilot_gz_description')
-    pkg_ros_gz_sim = get_package_share_directory(
-          'ros_gz_sim')
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
 
-    pkg_ardupilot_gazebo = get_package_share_directory(
-          'ardupilot_gazebo')
+    pkg_ardupilot_gazebo = get_package_share_directory("ardupilot_gazebo")
 
     # Load the SDF file from "description" package
     sdf_file = os.path.join(
-          pkg_ardupilot_gazebo,
-          'models',
-          'iris_with_standoffs',
-          'model.sdf')
-    with open(sdf_file, 'r') as infp:
+        pkg_ardupilot_gazebo, "models", "iris_with_standoffs", "model.sdf"
+    )
+    with open(sdf_file, "r") as infp:
         robot_desc = infp.read()
         # print(robot_desc)
 
     gz_sim_server = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
+        ),
         launch_arguments={
-            'gz_args':
-            "-v4 -s -r " + os.path.join(
-                pkg_project_gazebo,
-                'worlds',
-                'iris.sdf'
-            )
+            "gz_args": "-v4 -s -r "
+            + os.path.join(pkg_project_gazebo, "worlds", "iris.sdf")
         }.items(),
     )
 
     gz_sim_gui = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
-        launch_arguments={'gz_args': "-v4 -g"}.items(),
+            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
+        ),
+        launch_arguments={"gz_args": "-v4 -g"}.items(),
     )
 
     robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='both',
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
         parameters=[
-            {'use_sim_time': True},
-            {'robot_description': robot_desc},
-        ]
+            {"use_sim_time": True},
+            {"robot_description": robot_desc},
+        ],
     )
 
     # RViz
     rviz = Node(
-       package='rviz2',
-       executable='rviz2',
-       arguments=['-d', os.path.join(
-            pkg_project_bringup,
-            'config',
-            'iris.rviz')],
-       condition=IfCondition(LaunchConfiguration('rviz'))
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", os.path.join(pkg_project_bringup, "config", "iris.rviz")],
+        condition=IfCondition(LaunchConfiguration("rviz")),
     )
 
     # Bridge
     bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        parameters=[{
-            'config_file': os.path.join(
-                  pkg_project_bringup,
-                  'config',
-                  'iris_bridge.yaml'),
-            'qos_overrides./tf_static.publisher.durability': 'transient_local',
-        }],
-        output='screen'
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        parameters=[
+            {
+                "config_file": os.path.join(
+                    pkg_project_bringup, "config", "iris_bridge.yaml"
+                ),
+                "qos_overrides./tf_static.publisher.durability": "transient_local",
+            }
+        ],
+        output="screen",
     )
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'rviz',
-            default_value='true',
-            description='Open RViz.'),
-        robot_state_publisher,
-        bridge,
-        gz_sim_server,
-        gz_sim_gui,
-        rviz
-    ])
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "rviz", default_value="true", description="Open RViz."
+            ),
+            robot_state_publisher,
+            bridge,
+            gz_sim_server,
+            gz_sim_gui,
+            rviz,
+        ]
+    )
